359f008f091ded0f735566aa3c90f3d1728df249
rgbuckley1997@gmail.com <rgbuckley1997@gmail.com>
2024-09-05 20:37:15 +0000

    first comments

--- bandit.py
+++ bandit.py
@@ -742,11 +742,11 @@ def plot_optimal_rate_simulation(df):
     ax.fill_between(df_grp["trial_id"], df_grp["optimal_rate_min"], df_grp["optimal_rate_max"], color=gray, label='Std. Dev.')
 
     #y should be betwee 0 and 1
-    ax.set_ylim(0, 1) 
+    ax.set_ylim(0, 1.05) 
 
     #labels
     ax.set_xlabel('Trial ID')
-    ax.set_ylabel('optimal Rate')
+    ax.set_ylabel('Optimal Rate')
     ax.set_title('Estimated Cumulative Optimal Rate over Trials', fontsize=10, loc='left')
     ax.legend(fontsize=8)
 
@@ -920,11 +920,269 @@ def plot_top_goalkeeper_rate_simulation(df):
 
 # COMMAND ----------
 
+def plot_simulation_summary(df):
+    """
+    Given a dataframe output from a simulation, plot all relevant graphs
+    """
+
+    #set up grid
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(nrows=3, ncols=2, sharex=True, figsize=(12, 12))
+
+    # EXPLOIT RATE
+
+    #aggregate experiments
+    df_grp = (
+        df
+        .groupby('trial_id')
+        .agg(
+            exploit_rate_mu=('cumulative_exploit_rate', 'mean'),
+            exploit_rate_std=('cumulative_exploit_rate', 'std'),
+            target_exploit_rate=('target_exploit_rate', 'max')
+            )
+    )
+
+    #reset index
+    df_grp = df_grp.reset_index()
+
+    #calculate range
+    df_grp['exploit_rate_max'] = df_grp['exploit_rate_mu'] + df_grp['exploit_rate_std']
+    df_grp['exploit_rate_min'] = df_grp['exploit_rate_mu'] - df_grp['exploit_rate_std']
+
+    #plot average and target rates
+    ax1.plot(df_grp["trial_id"], df_grp["exploit_rate_mu"], label='Avg. Exploit Rate', color=black)
+    ax1.plot(df_grp["trial_id"], df_grp["target_exploit_rate"], label='Target Exploit Rate', color=orange, linestyle="--")
+
+    # Shade between standard deviation
+    ax1.fill_between(df_grp["trial_id"], df_grp["exploit_rate_min"], df_grp["exploit_rate_max"], color=gray, label='Std. Dev.')
+
+    #y should be betwee 0 and 1
+    ax1.set_ylim(0, 1) 
+
+    #labels
+    # ax1.set_xlabel('Trial ID')
+    ax1.set_ylabel('Exploit Rate')
+    ax1.set_title('Estimated Cumulative Exploit Rate over Trials', fontsize=10, loc='left')
+    ax1.legend(fontsize=8)
+
+    ## SAVE RATE
+
+    #aggregate experiments
+    df_grp = (
+        df
+        .groupby('trial_id')
+        .agg(
+            save_rate_mu=('cumulative_save_rate', 'mean'),
+            save_rate_std=('cumulative_save_rate', 'std'),
+            target_save_rate=('target_save_rate', 'max')
+            )
+    )
+
+    #reset index
+    df_grp = df_grp.reset_index()
+
+    #calculate range
+    df_grp['save_rate_max'] = df_grp['save_rate_mu'] + df_grp['save_rate_std']
+    df_grp['save_rate_min'] = df_grp['save_rate_mu'] - df_grp['save_rate_std']
+
+    #plot average and target rates
+    ax2.plot(df_grp["trial_id"], df_grp["save_rate_mu"], label='Avg. Save Rate', color=black)
+    ax2.plot(df_grp["trial_id"], df_grp["target_save_rate"], label='Target Save Rate', color=orange, linestyle="--")
+
+    #shade between standard deviation
+    ax2.fill_between(df_grp["trial_id"], df_grp["save_rate_min"], df_grp["save_rate_max"], color=gray, label='Std. Dev.')
+
+    #y should be betwee 0 and 1
+    ax2.set_ylim(0, 1) 
+
+    #labels
+    # ax2.set_xlabel('Trial ID')
+    ax2.set_ylabel('Save Rate')
+    ax2.set_title('Estimated Cumulative Save Rate over Trials', fontsize=10, loc='left')
+    ax2.legend(fontsize=8)
+
+    # OPTIMAL RATE
+
+    #aggregate experiments
+    df_grp = (
+        df
+        .groupby('trial_id')
+        .agg(
+            optimal_rate_mu=('cumulative_optimal_rate', 'mean'),
+            optimal_rate_std=('cumulative_optimal_rate', 'std'),
+            target_optimal_rate=('target_optimal_rate', 'max')
+            )
+    )
+
+    #reset index
+    df_grp = df_grp.reset_index()
+
+    #calculate range
+    df_grp['optimal_rate_max'] = df_grp['optimal_rate_mu'] + df_grp['optimal_rate_std']
+    df_grp['optimal_rate_min'] = df_grp['optimal_rate_mu'] - df_grp['optimal_rate_std']
+
+    #plot average and target rates
+    ax3.plot(df_grp["trial_id"], df_grp["optimal_rate_mu"], label='Avg. Optimal Rate', color=black)
+    ax3.plot(df_grp["trial_id"], df_grp["target_optimal_rate"], label='Target Optimal Rate', color=orange, linestyle="--")
+
+    #shade between standard deviation
+    ax3.fill_between(df_grp["trial_id"], df_grp["optimal_rate_min"], df_grp["optimal_rate_max"], color=gray, label='Std. Dev.')
+
+    #y should be betwee 0 and 1
+    ax3.set_ylim(0, 1.05) 
+
+    #labels
+    # ax3.set_xlabel('Trial ID')
+    ax3.set_ylabel('Optimal Rate')
+    ax3.set_title('Estimated Cumulative Optimal Rate over Trials', fontsize=10, loc='left')
+    ax3.legend(fontsize=8)
+
+    # P EST
+
+    #aggregate experiments
+    df_grp = (
+        df
+        .groupby('trial_id')
+        .agg(
+            Buffon_est_p_mu=('Buffon_est_p', 'mean'),
+            Buffon_est_p_std=('Buffon_est_p', 'std'),
+            Buffon_p=('Buffon_p', 'max'),
+            Casillas_est_p_mu=('Casillas_est_p', 'mean'),
+            Casillas_est_p_std=('Casillas_est_p', 'std'),
+            Casillas_p=('Casillas_p', 'max'),
+            Neuer_est_p_mu=('Neuer_est_p', 'mean'),
+            Neuer_est_p_std=('Neuer_est_p', 'std'),
+            Neuer_p=('Neuer_p', 'max'),
+            )
+    )
+
+    #reset index
+    df_grp = df_grp.reset_index()
+
+    #calculate range
+    df_grp['Buffon_p_max'] = df_grp['Buffon_est_p_mu'] + df_grp['Buffon_est_p_std']
+    df_grp['Buffon_p_min'] = df_grp['Buffon_est_p_mu'] - df_grp['Buffon_est_p_std']
+    df_grp['Casillas_p_max'] = df_grp['Casillas_est_p_mu'] + df_grp['Casillas_est_p_std']
+    df_grp['Casillas_p_min'] = df_grp['Casillas_est_p_mu'] - df_grp['Casillas_est_p_std']
+    df_grp['Neuer_p_max'] = df_grp['Neuer_est_p_mu'] + df_grp['Neuer_est_p_std']
+    df_grp['Neuer_p_min'] = df_grp['Neuer_est_p_mu'] - df_grp['Neuer_est_p_std']
+
+    #plot average and target rates
+    ax4.plot(df_grp["trial_id"], df_grp["Buffon_est_p_mu"], label='Avg. Buffon Estimated Save Rate', color=blue)
+    ax4.plot(df_grp["trial_id"], df_grp["Buffon_p"], label='Buffon True Save Rate', color=gray, linestyle="-.")
+
+    #shade between standard deviation
+    ax4.fill_between(df_grp["trial_id"], df_grp["Buffon_p_min"], df_grp["Buffon_p_max"], color=blue_shade, alpha=0.3, label='Buffon Std. Dev.')
+
+    #plot average and target rates
+    ax4.plot(df_grp["trial_id"], df_grp["Casillas_est_p_mu"], label='Avg. Casillas Estimated Save Rate', color=red)
+    ax4.plot(df_grp["trial_id"], df_grp["Casillas_p"], label='Casillas True Save Rate', color=gray, linestyle=":")
+
+    #shade between standard deviation
+    ax4.fill_between(df_grp["trial_id"], df_grp["Casillas_p_min"], df_grp["Casillas_p_max"], color=red_shade, alpha=0.3, label='Casillas Std. Dev.')
+
+    #plot average and target rates
+    ax4.plot(df_grp["trial_id"], df_grp["Neuer_est_p_mu"], label='Avg. Neuer Estimated Save Rate', color=teal)
+    ax4.plot(df_grp["trial_id"], df_grp["Neuer_p"], label='Neuer True Save Rate', color=gray, linestyle="--")
+
+    #shade between standard deviation
+    ax4.fill_between(df_grp["trial_id"], df_grp["Neuer_p_min"], df_grp["Neuer_p_max"], color=teal_shade, alpha=0.3, label='Neuer Std. Dev.')
+
+    #set the y axis as max of upper limit for any goalkeeper
+    y_max = df_grp[['Buffon_p_max', 'Casillas_p_max', 'Neuer_p_max']].max().max() +0.05
+    ax4.set_ylim(0, y_max) 
+
+    #labels
+    # ax4.set_xlabel('Trial ID')
+    ax4.set_ylabel('Save Rate')
+    ax4.set_title('Estimated Save Rates over Trials', fontsize=10, loc='left')
+    ax4.legend(fontsize=8, bbox_to_anchor=(1.02, 1), loc='upper left')
+
+    #TOP GOALKEEPER
+
+    #after each trial, count how many times we woulc pick each goalkeeper
+    df_grp = (
+        df
+        .groupby('trial_id')['top_goalkeeper']
+        .value_counts()
+        .unstack(fill_value=0)
+    )
+
+    #count number of experiments
+    df_grp['experiments'] = df_grp.sum(axis=1)
+
+    for gk in df_sim['top_goalkeeper'].unique():
+        df_grp[gk+'_rate'] = df_grp[gk] / df_grp['experiments']
+
+    df_grp = df_grp.reset_index()
+
+    #plot average and target rates
+    ax5.plot(df_grp["trial_id"], df_grp["Buffon_rate"], label='Buffon', color=blue)
+    ax5.plot(df_grp["trial_id"], df_grp["Casillas_rate"], label='Casillas', color=red)
+    ax5.plot(df_grp["trial_id"], df_grp["Neuer_rate"], label='Neuer', color=teal)
+
+    #y should be betwee 0 and 1
+    ax5.set_ylim(0, 1) 
+
+    #labels
+    ax5.set_xlabel('Trial ID')
+    ax5.set_ylabel('Rate Selected')
+    ax5.set_title('Selected Rate over Trials', fontsize=10, loc='left')
+    ax5.legend(fontsize=8)
+
+    # HEATMAP
+
+    # PREPARE DATAFRAME
+
+    #map gk_names to colors
+    colors = ['blue', 'red', 'teal']
+    color_map = {'Buffon': colors[0], 'Casillas': colors[1], 'Neuer': colors[2]}
+
+    #create df for heatmap. experiments are rows; columns are trials
+    experiment_ids = df['experiment_id'].unique()
+    trial_ids = df['trial_id'].unique()
+    heatmap_data = pd.DataFrame(index=experiment_ids, columns=trial_ids)
+
+    #fill heatmap using 'top_goalkeeper' column mapped to color
+    for index, row in df.iterrows():
+        goalkeeper = row['top_goalkeeper']
+        trial_id = row['trial_id']
+        experiment_id = row['experiment_id']
+        heatmap_data.loc[experiment_id, trial_id] = color_map[goalkeeper]
+
+    #convert colors to numeric values for heatmap
+    color_to_num = {color: i for i, color in enumerate(colors)}
+    heatmap_data_numeric = heatmap_data.replace(color_to_num)
+
+    # PLOTTING
+
+    sns.heatmap(heatmap_data_numeric, cmap=colors, cbar=False, ax=ax6)
+
+    #label every 10th experiment id
+    ax6.set_yticks(range(0, max(experiment_ids), 10))
+    ax6.set_yticklabels(range(0, max(experiment_ids), 10), rotation=0)
+    ax6.set_ylabel('Experiment ID')
+
+    #label every 100th trial id
+    ax6.set_xticks(range(0, max(trial_ids), 100))
+    ax6.set_xticklabels(range(0, max(trial_ids), 100), rotation=0)
+    ax6.set_xlabel('Trial ID')
+
+    #set title
+    ax6.set_title('Top Goalkeeper After Each Trial', fontsize=10, loc='left')
+    #add legend
+    handles = [plt.Rectangle((0,0),1,1, color=color_map[gk]) for gk in color_map]
+    ax6.legend(handles, color_map.keys(), title="Goalkeepers", bbox_to_anchor=(1.02, 1), loc='upper left')
+
+    plt.show()
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ### Run Simulation
 
 # COMMAND ----------
 
+#set parameters for the simulation
 num_experiments = 100
 num_trials = 1000
 eps = 0.1
@@ -933,9 +1191,33 @@ gk_names = ['Buffon', 'Casillas', 'Neuer']
 
 df_sim = simulation(num_experiments,num_trials,eps)
 
+#choose an experiment
+df_exp = df_sim[df_sim['experiment_id']==8]
+
 # COMMAND ----------
 
-df_exp = df_sim[df_sim['experiment_id']==33]
+# MAGIC %md
+# MAGIC The analyst runs his simulation with the following parameters:
+# MAGIC - We run each experiment for 1000 trials
+# MAGIC   - A "trial" is one shot
+# MAGIC - We simulate 100 experiments
+# MAGIC   - In real life, we will only do 1 experiment. Simulating 100 gives us a sense of how much we can trust the results from doing just one
+# MAGIC - Buffon has a save rate of 20%, Casillas 25%, and Neuer 30%.
+# MAGIC   - In real life, we won't know these beforehand, but setting them can guide our evaluation of our process
+# MAGIC   - We want our method to identify Neuer as the best goalkeeper as quickly as possible
+# MAGIC   - We also want our process to accurately predict the save rate of each goalkeeper 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC The coach and the analyst sit down to go over the results. The analyst starts by breaking down what happened in a single experiment.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This first chart shows the *exploit rate* over time. Remember, 10% of the time we *explore* by selecting a goalkeeper at random to face the shot. The other 90% of the time we *exploit* and have the goalkeeper with the best estimated save rate face the shot.  
+# MAGIC
+# MAGIC We can see that throughout our experiment, our exploit rate stayed very close to the expected 90%
 
 # COMMAND ----------
 
@@ -943,11 +1225,41 @@ plot_exploit_rate_experiment(df_exp)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC Next, we chart our save rate over time. Our *save rate* is the percent of times we have saved the shot _regardless of which goalkeeper faced the shot_. 
+# MAGIC
+# MAGIC Our target here is 30% - the highest save rate among all the goalkeepers. If Neuer, our best goalkeeper, faced every shot, we would expect a save rate of about 30%. 
+# MAGIC
+# MAGIC We can see that the longer the experiemnt goes on, the close we get to our target. This suggests that throughout the experiment we get better at identifying who the best goalkeeper is and selecting them to face the next shot when we exploit. We do, however, explore throughout the entire experiment, so with this method we would not expect to ever achieve a 30% save rate.
+
+# COMMAND ----------
+
 plot_save_rate_experiment(df_exp)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC Remember that Neuer is our best goalkeeper. This chart shows the percent of shots that Neuer faces throughout the experiement.
+# MAGIC
+# MAGIC Ideally, we would want our best goalkeeper to face every shot, so the target here is 100%. 
+# MAGIC
+# MAGIC We can see that for the first 400 shots, we only choose Neuer about 10% of the time. This suggests we thought a different goalkeeper was superior and when we *exploited* we did not choose Neuer. After about 400 kicks, however, we correctly identified him and began having him face more shots. 
+
+# COMMAND ----------
+
 plot_optimal_rate_experiment(df_exp)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This chart shows the true save rate and estimated save rate for each goalkeeper over time. The dashed lines are the true rates and the sold lines are our estimates.
+# MAGIC
+# MAGIC In this experiment, we can see that Buffon made a save early on while the others were scored on. This led us to believe Buffon had the best rate and have him face more shots. 
+# MAGIC
+# MAGIC Casillas then made a couple saves around 75 shots in. This led us to believe he was the best goalkeeper. He, however, failed to save subsequent shots and his estimated dropped back below Buffon. 
+# MAGIC
+# MAGIC Since we 
+# MAGIC
 
 # COMMAND ----------
 
@@ -980,3 +1292,7 @@ plot_top_goalkeeper_simulation(df_sim)
 # COMMAND ----------
 
 plot_top_goalkeeper_rate_simulation(df_sim)
+
+# COMMAND ----------
+
+plot_simulation_summary(df_sim)
